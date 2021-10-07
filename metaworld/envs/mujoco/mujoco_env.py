e394aea9f000fe3e3778a4fd40bfb9e806752341
vinnibuh <ivchenkov.yap@phystech.edu>
2021-10-08 00:31:39 +0300

    add segmentation in render

--- metaworld/envs/mujoco/mujoco_env.py
+++ metaworld/envs/mujoco/mujoco_env.py
@@ -118,7 +118,9 @@ class MujocoEnv(gym.Env, abc.ABC):
                 warnings.warn(str(err), category=RuntimeWarning)
                 self._did_see_sim_exception = True
 
-    def render(self, offscreen=False, camera_name="corner2", resolution=(640, 480)):
+    def render(self, offscreen=False, camera_name="corner2", resolution=(640, 480), segmentation=False):
+        # segmentation supported inly for offscreen=True
+        # TODO: add support for offscreen=False
         assert_string = ("camera_name should be one of ",
                 "corner3, corner, corner2, topview, gripperPOV, behindGripper")
         assert camera_name in {"corner3", "corner", "corner2", 
@@ -134,7 +136,8 @@ class MujocoEnv(gym.Env, abc.ABC):
             return self.sim.render(
                 *resolution,
                 mode='offscreen',
-                camera_name=camera_name
+                camera_name=camera_name,
+                segmentation=segmentation
             )
 
     def close(self):
